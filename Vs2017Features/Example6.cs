76712c8a05c021dc6d212bad65bfdaea2ecdc916
Germán Küber <german.kuber@outlook.com>
2018-06-12 14:44:06 -0300

    Updated

--- Vs2017Features/Example6.cs
+++ Vs2017Features/Example6.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace Vs2017Features
 {
     class Example6
     {
-        public Client Client { get; set; }
-        public Example6()
+        public Example6(HttpStatusCode code)
         {
-            Console.WriteLine(Client.Name);
+            if (2 > 1)
+                Console.WriteLine("Primero");
+            else
+                Console.WriteLine("Primero");
+
+
+            if (code == HttpStatusCode.OK)
+                Console.WriteLine("Ok" + code);
+            else if (code == HttpStatusCode.ExpectationFailed)
+                Console.WriteLine("ExpectationFailed");
+
         }
     }
 }
--- Vs2017Features/Example7.cs
+++ Vs2017Features/Example7.cs
@@ -5,21 +5,16 @@ using System.Net;
 
 namespace Vs2017Features
 {
+    
     class Example7
     {
-        public Example7(HttpStatusCode code)
+        public Example7(List<Person> people)
         {
-            if (2 > 1)
-                Console.WriteLine("Primero");
-            else
-                Console.WriteLine("Primero");
 
 
-            if (code == HttpStatusCode.OK)
-                Console.WriteLine("Ok" + code);
-            else if (code == HttpStatusCode.ExpectationFailed)
-                Console.WriteLine("ExpectationFailed");
+        }
 
+        public class Person {
         }
     }
 }
